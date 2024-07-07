e4f937f90cc7ab2f7ce16d3d50df377a9076604c
MrPurple6411 <mrpurple6411@gmail.com>
2024-07-06 18:40:16 -0600

    LMRR

---
+++ LargerMapRevealRadius/LargerMapRevealRadius.cs
@@ -0,0 +1,12 @@
+﻿namespace LargerMapRevealRadius;
+
+using HarmonyLib;
+
+public class LargerMapRevealRadius : IModApi
+{
+    public void InitMod(Mod mod)
+    {
+        Harmony.CreateAndPatchAll(typeof(Patches));
+        Log.Out("LargerMapRevealRadius Loaded");
+    }
+}
---
+++ LargerMapRevealRadius/Patches.cs
@@ -0,0 +1,25 @@
+﻿namespace LargerMapRevealRadius;
+
+using HarmonyLib;
+
+[HarmonyPatch]
+ public static class Patches
+{
+    [HarmonyPatch(typeof(IMapChunkDatabase), nameof(IMapChunkDatabase.Add), [typeof(Vector3i), typeof(World)]), HarmonyPrefix, HarmonyPriority(Priority.First)]
+    private static bool IMapChunkDatabase_Add_Prefix(IMapChunkDatabase __instance, Vector3i _chunkPos, World _world)
+    {
+        int num = 16;
+        for (int i = -num; i <= num; i++)
+        {
+            for (int j = -num; j <= num; j++)
+            {
+                Chunk chunk = (Chunk)_world.GetChunkSync(_chunkPos.x + i, _chunkPos.z + j);
+                if (chunk != null && !chunk.NeedsDecoration)
+                {
+                    __instance.Add(_chunkPos.x + i, _chunkPos.z + j, chunk.GetMapColors());
+                }
+            }
+        }
+        return false;
+    }
+}
